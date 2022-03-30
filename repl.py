64f2d595e8d73ae85f7aec3e5187384280b1f921
Will Bettridge-Radford <william.bettridge.radford@gmail.com>
2022-03-30 10:14:55 +0100

    update repl to use new c ish syntax

--- repl.py
+++ repl.py
@@ -1,34 +1,52 @@
 import talie
-prompt = "& "
+import uxndis
 
 def read_block():
     block = []
+    depth = 0
 
+    prompt = "& "
     while True:
         line = input(prompt)
+        prompt = "> "
 
         if line == '':
             break
 
+        depth += line.count('{')
+        depth -= line.count('}')
+
         if line:
             block.append(line)
 
+        if depth == 0:
+            break
+
     return block
+
 
 if __name__ == "__main__":
     rom = talie.UxnRom()
     rom.pc = 0x100
 
     while True:
-        block = read_block()
+        try:
+            block = read_block()
+        except KeyboardInterrupt:
+            exit()
+
         if not block:
             break
-        print("block")
+
+        # print("block")
+        pc = rom.pc
         data = '\n'.join(block) + '\n'
         talie.assemble(rom, data)
-        print(block)
-        print()
+        # print(block)
+        # print()
+        rom.resolve()
+        uxndis.disassemble(rom.rom, pc)
 
     rom.resolve()
-    print(rom)
+    uxndis.disassemble(rom.rom)
 
