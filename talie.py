cd9eeedfd3b43e0e886b767463c963caf0b3d56e
Will Bettridge-Radford <william.bettridge.radford@gmail.com>
2022-04-05 08:37:46 +0100

    misc

--- talie.py
+++ talie.py
@@ -80,16 +80,18 @@ class UxnRom():
         elif first_char == '.': # zero-page address
             self.make_reference(token, self.pc)
             self.write_lit_byte(0xff)
+        elif first_char == "'":
+            assert False # literal char
         elif first_char == '"':
             for b in bytes(token[1:], 'ascii'):
                 self.write_byte(b)
         elif first_char == '$':
             n = int(token[1:], 16)
             self.pc += n
+        elif first_char == '~':
+            assert False # todo include
         elif token[:3].lower() in op_table:
             self.write_op(token)
-        elif token == 'rpn':
-            pass
         else:
             n = int(token, 16)
             if n > 0xff:
